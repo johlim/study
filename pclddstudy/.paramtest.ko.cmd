177565c22c4d5ca841494b55f551447923f75b2a
johlim <johlim@daum.net>
2016-05-19 00:14:04 +0900

    clean

--- pclddstudy/.paramtest.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/limjh/lddstudy/paramtest.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-82-generic-pae/scripts/module-common.lds --build-id  -o /home/limjh/lddstudy/paramtest.ko /home/limjh/lddstudy/paramtest.o /home/limjh/lddstudy/paramtest.mod.o
--- pclddstudy/.tmp_versions/sk.mod
+++
@@ -1,2 +0,0 @@
-/home/limjh/lddstudy/sk.ko
-/home/limjh/lddstudy/sk.o
