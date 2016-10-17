96e5e82fc9684b4ea4cdb7d91d6fa474c5726864
johlim <johlim@daum.net>
2016-10-18 02:14:04 +0900

    유용한 util shell sample sdl src

---
+++ shellscript/backupD.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#tar -cf Mail.tar /media/Data/Mail
+#tar -cf Dropbox.tar /media/Data/Dropbox
+#tar -cf PC.tar 	/media/Data/PC
+#tar -cf PROJECT.tar /media/Data/PROJECT
+#tar -cf Nile.tar /media/Data/Nile
+#tar -cf install.tar /media/Data/install
+#tar -cf WinHex.tar /media/Data/WinHex
+#tar -cf Mydocuments.tar /media/Data/MyDocuments
+
+tar -cf StarTeam.tar /media/XP/StarTeam
+tar -cf T32.tar 	  /media/XP/T32
+tar -cf totalcmd.tar  /media/XP/totalcmd
+tar -cf utils.tar     /media/XP/utils
+tar -cf Corelogic.tar /media/XP/Corelogic
+tar -cf SWSetup.tar   /media/XP/SWSetup
+
---
+++ shellscript/mymount.sh
@@ -0,0 +1,1 @@
+/bin/mount -t nfs -o nolock 192.168.2.100:/nfs /mnt/nfs
