05874f53817b2c551c4ed6e29b528d127245a2d8
jhlim@dellserver <jhlim@dellserver>
2016-11-23 09:44:07 +0900

    update example

--- app/alarm/alarm_main.c
+++ app/alarm/alarm_main.c
@@ -28,10 +28,10 @@ void timer_prn_callback(int signum)
 		perror("timer error");
 	}
 	printf("The time is %s", ctime(&curtime));
-	tmbuf = (struct tm *)localtime(&curtime);
-	printf("The time is %s", asctime(tmbuf));
-	memcpy(&tmpbuf, tmbuf, sizeof(tmpbuf));
-	printf("The time is %d\n", tmpbuf.tm_sec);
+//	tmbuf = (struct tm *)localtime(&curtime);
+//	printf("The time is %s", asctime(tmbuf));
+//	memcpy(&tmpbuf, tmbuf, sizeof(tmpbuf));
+//	printf("The time is %d\n", tmpbuf.tm_sec);
 
 }
 
@@ -43,16 +43,21 @@ int main (int argc, char *argv)
 	memset(&Sa, 0, sizeof(Sa));
 	Sa.sa_handler = &timer_callback;
 	Sa.sa_handler = &timer_prn_callback;
-	sigaction(SIGPROF, &Sa, NULL);
+	//sigaction(SIGPROF, &Sa, NULL);
+	sigaction(SIGALRM, &Sa, NULL);
 
-	timer.it_value.tv_sec=1;
+	timer.it_value.tv_sec=3;
 	timer.it_value.tv_usec = 0;
-	timer.it_interval.tv_sec=1;
+	timer.it_interval.tv_sec=0;
 	timer.it_interval.tv_usec=0;
 
-	setitimer(ITIMER_PROF, &timer, NULL);
+	//setitimer(ITIMER_PROF, &timer, NULL);
+	setitimer(ITIMER_REAL, &timer, NULL);
 
-//	timer_prn_callback(0);
-	while(1);
+	timer_prn_callback(0);
+	while(1)
+	{
+		sleep(1);
+	}
 	return 0;
 }
