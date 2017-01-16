85d645d996fcb50886004dda9f8af2f5d3e2eca6
jhlim <johlim@gmail.com>
2017-01-16 23:51:00 +0900

    도전과제 qui slider 사용

--- py/training/tipcal.py
+++ py/training/tipcal.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 # -*- coding: cp949 -*-
-from future.utils import isint
-
 
 def isNumber(s):
-  try:
-    float(s)
-    return True
-  except ValueError:
-    print "invaild value"
-    return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        print "invaild value"
+        return False
+
 
 def isPositive(s):
-    if int(s) >= 0 :
+    if int(s) >= 0:
         return True
     else:
         print "negative value"
         return False
 
-def calc_tip(amount,tiprate_10):
+
+def calc_tip(amount, tiprate_10):
     tip = tiprate_10 / 100.0
-    return round(amount * tip,2)
+    return round(amount * tip, 2)
+
 
 def con_input_value():
     while True:
@@ -29,20 +30,18 @@ def con_input_value():
             break
     return amount
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print """-----------------------------"""
     print """ What is the bill amount ?"""
 
-    amount=con_input_value()
+    amount = con_input_value()
 
     print """ What is tip rate(percent)  """
-    try:
-        tiprate_10 = input()
-    except:
-        print """ wrong input """
-        print """ reenter rate """
 
-    tip = calc_tip(float(amount),float(tiprate_10))
+    tiprate_10 = con_input_value()
+
+    tip = calc_tip(float(amount), float(tiprate_10))
 
     print """ ***** total ***** """
     print """ tip %-0.2f $ """ % tip
--- py/training/tipcal_gui.py
+++ py/training/tipcal_gui.py
@@ -18,15 +18,25 @@ class MyWindow(QWidget):
         self.setWindowTitle("Tip calculator v0.1")
 
         self.pushButton = QPushButton("Start")
-        self.pushButton.clicked.connect(self.pushButtonClicked)
+        self.pushButton.clicked.connect(self.SliderMoved)
         self.label = QLabel()
+        self.slider = QtGui.QSlider()
+        self.slider.setMaximum(20)
+        self.slider.setMinimum(5)
+        self.slider.valueChanged.connect(self.showvalue)
+        self.slider.setGeometry(320, 100, 300, 240)
+
 
         layout = QVBoxLayout()
         layout.addWidget(self.pushButton)
         layout.addWidget(self.label)
+        layout.addWidget(self.slider)
+
 
         self.setLayout(layout)
         self.show()
+    def showvalue(self):
+        self.label.setText(str(self.slider.value()))
 
     def pushButtonClicked(self):
         text, ok = QInputDialog.getInt(self, 'Tip calculator', 'input amount')
@@ -40,6 +50,18 @@ class MyWindow(QWidget):
             strtmp3 = " sum {0:0.2f} $ ".format(float(tip) + float(amount))
             self.label.setText(str(strtmp1+"\n"+strtmp2+"\n"+strtmp3))
 
+    def SliderMoved(self):
+        text, ok = QInputDialog.getInt(self, 'Tip calculator', 'input amount')
+        if tipcal.isNumber(text) and tipcal.isPositive(text) and ok:
+            amount=text
+
+            text = self.slider.value()
+            tiprate_10=text
+            tip = tipcal.calc_tip(float(amount),float(tiprate_10))
+            strtmp1 = """ ***** total ***** """
+            strtmp2 = " tip {0:0.2f} $ ".format(tip)
+            strtmp3 = " sum {0:0.2f} $ ".format(float(tip) + float(amount))
+            self.label.setText(str(strtmp1+"\n"+strtmp2+"\n"+strtmp3))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
