0dc1ada32cd1ddd38c9ce9243d12c0a8a4a784bc
jhlim <johlim@gmail.com>
2017-02-14 00:12:33 +0900

    20170214 gstream pad

--- studygst/hello_gst/hello/main.c
+++ studygst/hello_gst/hello/main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <gst/gst.h>
 
+static GMainLoop *loop;
+
 int
 check_ver (int   argc,
       char *argv[])
@@ -27,7 +29,41 @@ check_ver (int   argc,
   return 0;
 }
 
+static gboolean
+my_bus_callback (GstBus     *bus,
+         GstMessage *message,
+         gpointer    data)
+{
+  g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
 
+  switch (GST_MESSAGE_TYPE (message)) {
+    case GST_MESSAGE_ERROR: {
+      GError *err;
+      gchar *debug;
+
+      gst_message_parse_error (message, &err, &debug);
+      g_print ("Error: %s\n", err->message);
+      g_error_free (err);
+      g_free (debug);
+
+      g_main_loop_quit (loop);
+      break;
+    }
+    case GST_MESSAGE_EOS:
+      /* end-of-stream */
+      g_main_loop_quit (loop);
+      break;
+    default:
+      /* unhandled message */
+      break;
+  }
+
+  /* we want to be notified again the next time there is a message
+   * on the bus, so returning TRUE (FALSE means we want to stop watching
+   * for messages on the bus and our callback should not be called again)
+   */
+  return TRUE;
+}
 
 int
 main (int   argc,
@@ -37,6 +73,8 @@ main (int   argc,
   GstElement *fileter_element;
   GstElement *pad_element;
   GstElement *pipeline;
+  GstBus *bus;
+  guint bus_watch_id;
 
   /* init GStreamer */
   gst_init (&argc, &argv);
@@ -72,13 +110,20 @@ main (int   argc,
 
   gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PLAYING);
 
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  bus_watch_id = gst_bus_add_watch (bus, my_bus_callback, NULL);
+  gst_object_unref (bus);
+
+
+
+  //sleep(1);
+  loop = g_main_loop_new (NULL, FALSE);
+  g_main_loop_run (loop);
+
   gst_object_unref (GST_OBJECT (pipeline));
   gst_object_unref (GST_OBJECT (pad_element));
   gst_object_unref (GST_OBJECT (fileter_element));
   gst_object_unref (GST_OBJECT (source_element));
-
-  sleep(1);
-
   return 0;
 }
 
--- studygst/hello_gst/mp3player/b.sh
+++
@@ -1,5 +0,0 @@
-gst-launch-1.0 filesrc location="sample_staticimage.m2v" ! typefind ! queue ! fakesink
-
-#tsdemux name=demux demux. ! queue ! fakesink
-#mpeg2dec ! fakesink
-#demux. ! ’audio/mpeg’ ! queue ! avdec ! volume volume=5 ! alsasink
