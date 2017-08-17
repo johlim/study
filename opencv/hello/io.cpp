b7d9c08396c23748306820068ca5fffebd1fc345
jhlim <jhlim@plk.co.kr>
2017-08-17 23:13:14 +0900

    build ok

--- opencv/hello/io.cpp
+++ opencv/hello/io.cpp
@@ -1,12 +1,14 @@
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 int main()
 {
     cv::Mat img;
+    cv::Mat dst;
     cv::VideoCapture input(0);
     cv::VideoWriter output(
             "cars_sobel.avi",
-            CV_FOURCC('X','V','I','D');
+            CV_FOURCC('X','V','I','D'),
             30,
             cv::Size(input.get(CV_CAP_PROP_FRAME_WIDTH),
                 input.get(CV_CAP_PROP_FRAME_HEIGHT)));
@@ -21,7 +23,7 @@ int main()
 
         output.write(dst);
         cv::imshow("img",img);
-        char c = cv::waitkey(30);
+        char c = cv::waitKey(30);
 
         if (c == ' ')
             break;
