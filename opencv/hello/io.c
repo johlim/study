c515a815e3f41744cebeaf0f8f370cb5294a4de0
johlim <johlim@gmail.com>
2017-08-17 22:56:08 +0900

    add opencv example

---
+++ opencv/hello/io.c
@@ -0,0 +1,29 @@
+#include <opencv2/highgui/highgui.hpp>
+
+int main()
+{
+    cv::Mat img;
+    cv::VideoCapture input(0);
+    cv::VideoWriter output(
+            "cars_sobel.avi",
+            CV_FOURCC('X','V','I','D');
+            30,
+            cv::Size(input.get(CV_CAP_PROP_FRAME_WIDTH),
+                input.get(CV_CAP_PROP_FRAME_HEIGHT)));
+
+    for(;;)
+    {
+        if(!input.read(img))
+            break;
+        cv::Sobel(img, dst, CV_8U,1,1);
+        output.write(dst);
+        cv::imshow("img",img);
+
+        output.write(dst);
+        cv::imshow("img",img);
+        char c = cv::waitkey(30);
+
+        if (c == ' ')
+            break;
+    }
+}
