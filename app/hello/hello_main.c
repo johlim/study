bfab30be3d84a946039a59de37ef8c54c4e6b1f8
johlim <johlim@gmail.com>
2020-06-11 10:13:50 +0000

    update hello world

--- app/hello/hello_main.c
+++ app/hello/hello_main.c
@@ -1,8 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+static int str_match_string( char* key, char* target )
+{
+	int srclen=strlen(target);
+	int len=strlen(key);
+	int iret=0;
+
+	printf ("%s %s comp %s\n", __func__, key, target);
+
+	if ((srclen-len) <= 0)
+		return iret;
+	for(int i=0; i < srclen-len; i++ )
+	{
+		iret= memcmp(key,target+i,len);
+		printf ("%s %d %s comp %s\n", __func__, iret, key, target+i);
+		if (iret == 0)
+			break;
+	}
+
+	return iret;
+}
 int main(int argc, char * argv)
 {
-	printf("hello world \n");
+	printf("hello world %d \n", str_match_string("20200611","/mnt/config/20200611_product.cfg"));
 	return 0;
 }
