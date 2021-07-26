6a6ffcc409d91072a8911d7b025c2f7ccbdc5464
johlim <johlim@gmail.com>
2021-07-26 17:02:49 +0900

    add Makefile and update

--- app/time/time_diff.c
+++ app/time/time_diff.c
@@ -1,6 +1,7 @@
 #include <time.h>
+#include <stdio.h>
 
-int main(int argc, char * argv[])
+int print_time(int argc, char * argv[])
 {
 	time_t the_time;
 	struct tm* timeinfo;
@@ -10,5 +11,13 @@ int main(int argc, char * argv[])
 	time(&the_time);
 	timeinfo = localtime(&the_time);
 	iReadTimeInterval = uAccTimer + 900;
+	printf("%lld \n", iReadTimeInterval);	
 
 }	
+int main(int argc, char * argv[])
+{
+	int i=0;
+	for(i=0; i<10; i++)
+		print_time(0,NULL);
+	return 0;
+}
