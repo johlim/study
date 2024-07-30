cf48c067167bfb911281ad4f6d8b84fceb262614
peter.lim <peter.lim@aimatics.ai>
2024-07-30 22:36:20 +0900

    mod for python 3.x

--- IOT/weather.py
+++ IOT/weather.py
@@ -1,9 +1,10 @@
 import feedparser
 
-feed_url="http://w1.weather.gov/xml/current_obs/KPVD.rss"
+#feed_url="http://w1.weather.gov/xml/current_obs/KPVD.rss"
+feed_url="https://coinpipe.tistory.com/rss"
 feed = feedparser.parse(feed_url)
 RSSitems = feed["items"]
 for item in RSSitems:
     weather = item["title"]
-    print weather
+    print (weather)
 
